191ba92adedb230e24395a356cf3f6f9e854477e
yusiou <yusiou@g.ucla.edu>
2017-04-18 15:34:17 -0700

    finished p3, edited p2d

--- HW_1/HW1_p3.R
+++ HW_1/HW1_p3.R
@@ -10,13 +10,13 @@ g <- barabasi.game(nodesNum , directed = FALSE)
 
 plot(g, vertex.size=8, vertex.label=NA)
 
-dg_dist = degree_distribution(g)
-barplot(dg_dist)
-
+dg_dist = degree(g)
+hist(deg_dist, main="Degree Distribution (3a)", xlab="Degree Number", ylab="Probability")
 
 # find communities
 fg <- fastgreedy.community(g)
 membership(fg)
 cmsize <- sizes(fg)
 
+barplot(cmsize,  main="Community Sizes (3b)", xlab="Community Number", ylab="Community Size")
 modularity(fg, membership(fg))
--- Katie_workspace/Katie_HW1.R
+++ Katie_workspace/Katie_HW1.R
@@ -4,53 +4,53 @@
 ##Load package
 library(igraph)
 
-p1 = 0.01
-g <- sample_gnp(1000, p1, directed = FALSE, loops = FALSE)
-plot(g, vertex.size = 2)
-deg_dist <- degree_distribution(g)
-plot(0: (length(deg_dist)-1), deg_dist)
-is.connected(g)
-dia <- diameter(g, unconnected = TRUE)
+nodesNum = 1000
 
 #2 
 #Create a network with a fat-tailed degree distribution
 #2-a
-g2a = barabasi.game(1000, -3, directed = FALSE)
-deg_dist2a <- degree_distribution(g2a)
-plot(deg_dist2a)
-dia2a <- diameter(g2a, unconnected = TRUE)
+
+degreePro = -3
+
+g_2a = barabasi.game(nodesNum, degreePro, directed = FALSE)
+deg_dist_2a <- degree(g_2a)
+hist(deg_dist_2a, main="Degree Distribution (2a)", xlab="Degree Number", ylab="Probability")
+dia_2a <- diameter(g_2a, unconnected = TRUE)
 
 #2-b
-is.connected(g2a)
-cl <- clusters(g2a)
-gccIndex <- which.max(cl$size)
-nonGccNodes <- (1:vcount(g))[cl$membership != gccIndex]
-gcc <- delete.vertices(g, nonGccNodes)
+is.connected(g_2a)
+cl_2a <- clusters(g_2a)
+gccIndex_2a <- which.max(cl_2a$size)
+nonGccNodes_2b <- (1:vcount(g_2a))[cl_2a$membership != gccIndex_2a]
+gcc_2b <- delete.vertices(g_2a, nonGccNodes_2b)
 
 #community structure
-community_str <- fastgreedy.community(graph, merges=TRUE, modularity=TRUE,membership=TRUE, weights=NULL)
-modularity = modularity(community_str)
-size = sizes(community_str)
-
+community_str_2b <- fastgreedy.community(gcc_2b, merges=TRUE, modularity=TRUE,membership=TRUE, weights=NULL)
+modularity(community_str_2b)
+sizes(community_str_2b)
+barplot(sizes(community_str_2b),  main="Community Sizes (2b)", xlab="Community Number", ylab="Community Size")
 
 #2-c
-g2c = barabasi.game(10000, -3, directed = FALSE)
-deg_dist2c <- degree_distribution(g2c)
-barplot(deg_dist2c)
-dia2c <- diameter(g2c, unconnected = TRUE)
+
+nodesNum2 = 10000
+
+g_2c = barabasi.game(nodesNum2, degreePro, directed = FALSE)
+deg_dist_2c <- degree(g_2c)
+hist(deg_dist_2c, main="Degree Distribution (2c)", xlab="Degree Number", ylab="Probability")
+dia_2c <- diameter(g_2c, unconnected = TRUE)
 
 #GCC calculation
 
-cl <- clusters(g2c)
-gccIndex <- which.max(cl$size)
-nonGccNodes <- (1:vcount(g))[cl$membership != gccIndex]
-gcc <- delete.vertices(g, nonGccNodes)
+cl_2c <- clusters(g_2c)
+gccIndex_2c <- which.max(cl_2c$size)
+nonGccNodes_2c <- (1:vcount(g))[cl_2c$membership != gccIndex_2c]
+gcc_2c <- delete.vertices(g_2c, nonGccNodes_2c)
 
 #community structure
-community_str <- fastgreedy.community(gcc, merges=TRUE, modularity=TRUE,membership=TRUE, weights=NULL)
-modularity = modularity(community_str)
-size = sizes(community_str)
-
+community_str_2c <- fastgreedy.community(gcc_2c, merges=TRUE, modularity=TRUE,membership=TRUE, weights=NULL)
+modularity(community_str_2c)
+sizes(community_str_2c)
+barplot(sizes(community_str_2c),  main="Community Sizes (2c)", xlab="Community Number", ylab="Community Size")
 
 #2-d
 smpl <- igraph.sample(1, 1000, 1000)
