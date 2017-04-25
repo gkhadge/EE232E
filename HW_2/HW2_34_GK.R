17f6b56b083f5a0e79508f2de764edfc023122a9
eunsunlee <eunsunlee@ucla.edu>
2017-04-25 15:36:09 -0700

    problem 3 and problem 4 a,b

--- HW_2/HW2_34_GK.R
+++ HW_2/HW2_34_GK.R
@@ -9,7 +9,7 @@ numSteps <- 1000000
 
 # regenerate graph until it is connected
 g_3a <- sample_gnp(numNodes, p, directed = FALSE)
-while (!is.connected(g_3b))
+while (!is.connected(g_3a))
 {
   g_3a <- sample_gnp(numNodes, p, directed = FALSE)
   print("Not Connected, Regenerating")
@@ -25,7 +25,7 @@ pr <- q/numSteps
 deg_g_3a <- degree(g_3a)
 # Plot against each other
 plot(deg_g_3a,pr,xlim=range(0,max(deg_g_3a)),ylim=range(0,max(pr)), main="3(a) Probability of visit vs. Degree of each node", ylab="Probability of visit", xlab="Degree of node")
-
+cor(deg_g_3a, pr)
 
 #Problem 3 (b)
 # Parameters
@@ -50,9 +50,12 @@ pr <- q/numSteps
 # Find degree of each node
 deg_g_3b_out <- degree(g_3b,mode = "out")
 deg_g_3b_in <- degree(g_3b,mode = "in")
+deg_g_3b_total <- degree(g_3b)
 # Plot against each other
 plot(deg_g_3b_out,pr[1:length(deg_g_3b_out)],xlim=range(0,max(deg_g_3b_out)),ylim=range(0,max(pr)), main="3(b) Probability of visit vs. Outgoing Degree of each node", ylab="Probability of visit", xlab="Outgoing Degree of node")
+cor(pr[1:length(deg_g_3b_out)],deg_g_3b_out)
 plot(deg_g_3b_in,pr[1:length(deg_g_3b_in)],xlim=range(0,max(deg_g_3b_in)),ylim=range(0,max(pr)), main="3(b) Probability of visit vs. Incoming Degree of each node", ylab="Probability of visit", xlab="Incoming Degree of node")
+cor(deg_g_3b_in,pr[1:length(deg_g_3b_in)])
 
 #problem 3 (c)
 # Random Walk and Teleport Params
@@ -106,7 +109,7 @@ pr <- q/currStep
 deg_g_3c <- degree(g_3c)
 # Plot against each other
 plot(deg_g_3c,pr,xlim=range(0,max(deg_g_3c)),ylim=range(0,max(pr)), main="3(c) Probability of visit vs. Degree of each node", ylab="Probability of visit", xlab="Degree of node")
-
+cor(deg_g_3c, pr)
 
 
 #problem 4 (a)
