20779465addb81571ddbac562a4deb069622ef88
yusiou <yusiou@g.ucla.edu>
2017-04-27 16:53:41 -0700

    trying to simplify gcc

--- Yusi_workspace/HW3_12.R
+++ Yusi_workspace/HW3_12.R
@@ -8,9 +8,18 @@ is.directed(g)
 
 is.connected(g)
 cl <- clusters(g)
-gccIndex <- which.max(cl$size)
+gccIndex <- which.max(cl$csize)
 nonGccNodes <- (1:vcount(g))[cl$membership != gccIndex]
 gcc <- delete.vertices(g, nonGccNodes)
+length(V(gcc))
 
-gcc_dg <- degree_distribution(g, mode = "out")
-plot()
+gcc_dg_in <- degree(gcc, mode = "out")
+gcc_hist_in <- hist(gcc_dg_in, plot = "FALSE")
+plot(gcc_hist_in$count, log = "y", type = "h", lwd = 10, lend = 2, main = "Outgoing Degree Distribution", xlab = "Degree", ylab = "Number of Nodes")
+
+gcc_dg_out <- degree(gcc, mode = "in")
+gcc_hist_out <- hist(gcc_dg_out, plot = "FALSE")
+plot(gcc_hist_out$count, log = "y", type = "h", lwd = 10, lend = 2, main = "Incoming Degree Distribution", , xlab = "Degree", ylab = "Number of Nodes")
+
+gcc_ud <- as.undirected(gcc, mode = "each")
+simplify(gcc_ud, remove.multiple = TRUE, remove.loops = TRUE)
