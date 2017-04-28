be0b95f0160b0c3686f2b5e67c70c5b5d993e541
gkhadge <gkhadge@g.hmc.edu>
2017-04-27 18:42:11 -0700

    added material for 4 and 5

--- Gourav_workspace/HW3.R
+++ Gourav_workspace/HW3.R
@@ -38,3 +38,18 @@ gcc_ud2_comm_FG <- fastgreedy.community(gcc_ud2)
 gcc_ud2_comm_LP <- label.propagation.community(gcc_ud2) 
 
 # Note LP is awful, use FG when possible
+
+# TODO: PLEASE VERIFY CODE
+# Extract Largest Community form UD2_FG from 
+largest_comm_index <- which.max(sizes(gcc_ud2_comm_FG))
+nonLCNodes <- (1:vcount(gcc))[gcc_ud2_comm_FG$membership != largest_comm_index]
+gcc_LC <- delete.vertices(gcc, nonLCNodes) #GCC Largest Community
+# plot(gcc_LC, vertex.size=5, vertex.label.cex=0.4,edge.arrow.size=0.2)
+
+
+# Convert gcc_LC to undirected graph (Option 2)
+gcc_LC_ud <- as.undirected(gcc_LC,mode="collapse",edge.attr.comb = "prod")
+E(gcc_LC_ud)$weight <- sqrt(E(gcc_LC_ud)$weight)
+
+# Find Communities of subnet using FG
+gcc_LC_ud_FG <- fastgreedy.community(gcc_LC_ud)
