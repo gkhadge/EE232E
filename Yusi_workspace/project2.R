3038eab79a049521486335bcacf5c8c6126d7091
yusiou <yusiou@g.ucla.edu>
2017-06-01 14:45:29 -0700

    updated project 2

--- Yusi_workspace/project2.R
+++ Yusi_workspace/project2.R
@@ -19,8 +19,28 @@ BvS_neigh <- neighbors(g_movie, v = BvS)
 MI_neigh <- neighbors(g_movie, v = MI)
 M_neigh <- neighbors(g_movie, v = M)
 
+##### Problem 7: Ratings
 
-#### BvS Top 5 Neighbors #####
+# Read movie rating data
+rating_data <- list()
+rating_con <- file("/Users/Yusi/Documents/EE232E/Project2/project_2_data/movie_rating.txt", "r")
+rating_data_raw <- readLines(rating_con)
+close(rating_con)
+
+rating_data <- strsplit(rating_data_raw,"\t\t", perl = TRUE)
+rating_data_names <- c()
+rating_data_vals <- c()
+
+for (i in 1:length(rating_data)){
+  rating_data_names[i] <- gsub(" ", "", rating_data[[i]][1], fixed = TRUE)
+  rating_data_vals[i] <- rating_data[[i]][2]
+}
+rating_data_vals <- as.numeric(rating_data_vals)
+rating_data_list <- setNames(object = rating_data_vals, rating_data_names)
+
+
+#### BvS Top Neighbors #####
+n_neigh <- 20
 
 BvS_edgelist <- c()
 
@@ -34,20 +54,21 @@ BvS_weights <- E(g_movie, BvS_edgelist)$weight
 BvS_ind <- c()
 BvS_weights_temp <- BvS_weights
 
-for (i in 1:5){
+for (i in 1:n_neigh){
   BvS_ind <- c(BvS_ind, which.max(BvS_weights_temp))
   BvS_weights_temp[which.max(BvS_weights_temp)]=0
 }
 
-BvS_top5 <- BvS_neigh[BvS_ind]
+BvS_top <- BvS_neigh[BvS_ind]
 
-BvS_comm_top5 <- c(membership(g_movie_fg)[BvS])
+#BvS_comm_top <- c(membership(g_movie_fg)[BvS])
+BvS_comm_top <- c()
 
-for (i in 1:length(BvS_top5)){
-  BvS_comm_top5 <- c(BvS_comm_top5, membership(g_movie_fg)[BvS_top5[i]])
+for (i in 1:length(BvS_top)){
+  BvS_comm_top <- c(BvS_comm_top, membership(g_movie_fg)[BvS_top[i]])
 }
 
-#### MI Top 5 Neighbors #####
+#### MI Top Neighbors #####
 
 MI_edgelist <- c()
 
@@ -61,20 +82,21 @@ MI_weights <- E(g_movie, MI_edgelist)$weight
 MI_ind <- c()
 MI_weights_temp <- MI_weights
 
-for (i in 1:5){
+for (i in 1:n_neigh){
   MI_ind <- c(MI_ind, which.max(MI_weights_temp))
   MI_weights_temp[which.max(MI_weights_temp)]=0
 }
 
-MI_top5 <- MI_neigh[MI_ind]
+MI_top <- MI_neigh[MI_ind]
 
-MI_comm_top5 <- c(membership(g_movie_fg)[MI])
+#MI_comm_top <- c(membership(g_movie_fg)[MI])
+MI_comm_top <- c()
 
-for (i in 1:length(MI_top5)){
-  MI_comm_top5 <- c(MI_comm_top5, membership(g_movie_fg)[MI_top5[i]])
+for (i in 1:length(MI_top)){
+  MI_comm_top <- c(MI_comm_top, membership(g_movie_fg)[MI_top[i]])
 }
 
-#### M Top 5 Neighbors #####
+#### M Top Neighbors #####
 M_edgelist <- c()
 
 for (i in 1:length(M_neigh)){
@@ -87,27 +109,52 @@ M_weights <- E(g_movie, M_edgelist)$weight
 M_ind <- c()
 M_weights_temp <- M_weights
 
-for (i in 1:5){
+for (i in 1:n_neigh){
   M_ind <- c(M_ind, which.max(M_weights_temp))
   M_weights_temp[which.max(M_weights_temp)]=0
 }
 
-M_top5 <- M_neigh[M_ind]
+M_top <- M_neigh[M_ind]
 
-M_comm_top5 <- c(membership(g_movie_fg)[M])
+#M_comm_top <- c(membership(g_movie_fg)[M])
+M_comm_top <- c()
 
-for (i in 1:length(MI_top5)){
-  M_comm_top5 <- c(M_comm_top5, membership(g_movie_fg)[M_top5[i]])
+for (i in 1:length(MI_top)){
+  M_comm_top <- c(M_comm_top, membership(g_movie_fg)[M_top[i]])
 }
 
-BvS_comm_top5
-MI_comm_top5
-M_comm_top5
-
-# Read movie rating data
-rating_data <- list()
-rating_con <- file("/Users/Yusi/Documents/EE232E/Project2/project_2_data/movie_rating.txt", "r")
-rating_data <- readLines(rating_con)
-close(rating_con)
+BvS_comm_top
+MI_comm_top
+M_comm_top
 
 
+######## Problem 7: Ratings
+
+
+BvS_top_rating <- c()
+for (i in 1:length(BvS_ind)){
+  BvS_top_rating <- c(BvS_top_rating, rating_data_list[BvS_top[i]$name])
+}
+
+MI_top_rating <- c()
+for (i in 1:length(M_ind)){
+  MI_top_rating <- c(MI_top_rating, rating_data_list[MI_top[i]$name])
+}
+
+M_top_rating <- c()
+for (i in 1:length(M_ind)){
+  M_top_rating <- c(M_top_rating, rating_data_list[M_top[i]$name])
+}
+
+BvS_top
+BvS_top_rating
+
+MI_top
+MI_top_rating
+
+M_top
+M_top_rating
+
+save(BvS_top_rating,file="/Users/Yusi/Documents/EE232E/Project2/BvS_top_rating.RData")
+save(MI_top_rating,file="/Users/Yusi/Documents/EE232E/Project2/MI_top_rating.RData")
+save(M_top_rating,file="/Users/Yusi/Documents/EE232E/Project2/M_top_rating.RData")
