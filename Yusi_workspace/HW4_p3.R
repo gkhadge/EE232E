8142c518fc7fd74c30f5b2e485268b6cb1d689a8
yusiou <yusiou@g.ucla.edu>
2017-06-13 00:23:31 -0700

    finished 7

--- Yusi_workspace/HW4_p3.R
+++ Yusi_workspace/HW4_p3.R
@@ -3,8 +3,12 @@
 rm(list=ls())
 library(igraph)
 
+##################################################################################################
+# Problem 3
+##################################################################################################
+
 # Load the stock edgelist
-stock_file<-file("/Users/Yusi/Documents/EE232E/HW_4/StockNetworkFile.txt")
+stock_file <- file("/Users/Yusi/Documents/EE232E/HW_4/StockNetworkFile.txt")
 sector_file <- read.csv("/Users/Yusi/Documents/EE232E/HW_4/finance_data/Name_sector.csv", header = TRUE, sep = ",", quote = "\"")
 
 # create stock graph
@@ -14,23 +18,25 @@ n_stocks <- length(V(g_stocks))
 n_sectors <- 11
 
 g_mst <- mst(g_stocks)
-plot(g_mst, vertex.label = NA, vertex.size = 2)
+plot(g_mst, vertex.label = NA, vertex.size = 5)
 
 sector_list <- setNames(object = sector_file$Sector, sector_file$Symbol)
 
 sector_color <- rep(0, n_stocks)
+colors_s <- rainbow(n_sectors)
+colors_s[3] = "#FFFF00"
 
-sector_color[which(sector_list == "Health Care")] <- colors[1]
-sector_color[which(sector_list == "Industrials")] <- colors[2]
-sector_color[which(sector_list == "Consumer Discretionary")] <- colors[3]
-sector_color[which(sector_list == "Information Technology")] <- colors[4]
-sector_color[which(sector_list == "Consumer Staples")] <- colors[5]
-sector_color[which(sector_list == "Utilities")] <- colors[6]
-sector_color[which(sector_list == "Financials")] <- colors[7]
-sector_color[which(sector_list == "Materials")] <- colors[8]
-sector_color[which(sector_list == "Energy")] <- colors[9]
-sector_color[which(sector_list == "Telecommunication Services")] <- colors[10]
-sector_color[which(sector_list == "Real Estate")] <- colors[11]
+sector_color[which(sector_list == "Health Care")] <- colors_s[1]
+sector_color[which(sector_list == "Industrials")] <- colors_s[2]
+sector_color[which(sector_list == "Consumer Discretionary")] <- colors_s[3]
+sector_color[which(sector_list == "Information Technology")] <- colors_s[4]
+sector_color[which(sector_list == "Consumer Staples")] <- colors_s[5]
+sector_color[which(sector_list == "Utilities")] <- colors_s[6]
+sector_color[which(sector_list == "Financials")] <- colors_s[7]
+sector_color[which(sector_list == "Materials")] <- colors_s[8]
+sector_color[which(sector_list == "Energy")] <- colors_s[9]
+sector_color[which(sector_list == "Telecommunication Services")] <- colors_s[10]
+sector_color[which(sector_list == "Real Estate")] <- colors_s[11]
 
 sector_color_list <- setNames(object = sector_color, sector_file$Symbol)
 
@@ -43,3 +49,27 @@ for (i in 1:n_stocks){
 }
 
 plot(g_mst, vertex.label = NA, vertex.color=sector_sorted_colors, vertex.size = 5, main = "Daily MST")
+
+
+##################################################################################################
+# Problem 7: Modifying Correlation
+##################################################################################################
+
+# Load the stock edgelist
+stock_file_mod <- file("/Users/Yusi/Documents/EE232E/HW_4/StockNetworkFileMod.txt")
+
+# create stock graph
+g_stocks_mod <- read_graph(stock_file_mod,format="ncol",directed=FALSE)
+
+g_mst_mod <- mst(g_stocks_mod)
+plot(g_mst_mod, vertex.label = NA, vertex.size = 5)
+
+sector_sorted_names_mod <- rep(0, n_stocks)
+sector_sorted_colors_mod <- rep(0, n_stocks)
+
+for (i in 1:n_stocks){
+  sector_sorted_names_mod[i] <- V(g_mst_mod)[i]$name
+  sector_sorted_colors_mod[i] <- sector_color_list[V(g_mst_mod)[i]$name]
+}
+
+plot(g_mst_mod, vertex.label = NA, vertex.color=sector_sorted_colors_mod, vertex.size = 5, main = "Daily MST (Modified)")
